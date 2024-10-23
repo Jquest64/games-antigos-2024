2495e649e81223dcca0b7ad83285a16cecb17b30
Marcio Farias Pinto <mjquest64@outlook.com>
2024-10-22 22:52:12 -0300

    Início do Projeto

---
+++ src/main/java/com/games/GamesAntigos2024Application.java
@@ -0,0 +1,13 @@
+package com.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesAntigos2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesAntigos2024Application.class, args);
+	}
+
+}
